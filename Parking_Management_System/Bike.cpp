3d42d09ecf34f5ce00140aa1e98fc417ef794792
Dhruv Mangroliya <dhruvmangroliya@192.168.1.2>
2025-09-20 06:43:24 +0530

    Initial Commit

--- Parking_Management_System/Bike.cpp
+++
@@ -1,7 +0,0 @@
-#include "Bike.hpp"
-
-using namespace std;
-
-Bike::Bike(string license) : Vehicle(license) {
-    cout<<"Bike created with License: "<<license<<endl;
-} 
--- Parking_Management_System/Car.cpp
+++
@@ -1,7 +0,0 @@
-#include "Car.hpp"
-
-using namespace std;
-
-Car::Car(string license) : Vehicle(license) {
-    cout<<"Car created with License: "<<license<<endl;
-} 
