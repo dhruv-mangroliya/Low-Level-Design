4001ace07cad2b888544dd312e31374c94dd50c0
dhruvmangroliya <dhruvmangroliya642@gmail.com>
2025-09-26 20:24:39 +0530

    Added Tic Tac Toe project from old repo

--- Java/Tic Tac Toe/src/main/java/org/example/TTTDemo.java
+++ Java/Tic Tac Toe/src/main/java/org/example/TTTDemo.java
@@ -1,37 +1,15 @@
 package org.example;
 
 import org.example.Board.Board;
+import org.example.Game.Game;
+import org.example.Game.TTTGame;
 import org.example.Player.Player;
+import org.example.Player.PlayerFactory;
 
-public class TTTDemo {
+public class TTTDemo{
     public static void main(String[] args) {
-        Board board = Board.getInstance();
-        Player p1 = new Player('O');
-        Player p2 = new Player('X');
-
-        board.setFirstPlayer(p1);
-
-        board.makeMove(p1, 1);
-        board.showBoard();
-
-        board.makeMove(p2, 2);
-        board.showBoard();
-
-        board.makeMove(p1, 4);
-        board.showBoard();
-
-        board.makeMove(p2, 5);
-        board.showBoard();
-
-        board.makeMove(p1, 7);
-        board.showBoard();
-
-        board.makeMove(p1, 8);
-        board.showBoard();
-
-        board.makeMove(p2, 8);
-        board.showBoard();
-
+        TTTGame game = new TTTGame();
+        game.play();
         return;
     }
 }
